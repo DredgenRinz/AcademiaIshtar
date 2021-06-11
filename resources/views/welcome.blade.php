f4b4a7b8df70d69c4971f829334c3f589f4da733
DredgenRinz <javier.vargas2020@twk.cl>
2021-06-11 17:42:35 -0400

    modificando pagina inicio y redirects sin login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,82 +21,20 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen sm:items-center" style="background-image: url(https://www.bungie.net/7/ca/destiny/products/B01938E97B95/b2CB63E40306EC_d.jpg); background-size: cover">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="text-md text-gray-700 underline">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-md dark:text-white underline">Ingresar</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-md dark:text-white underline">Registrar</a>
-                        @endif
-                    @endif
-                </div>
-            @endif
-
-            <div class="max-w-6xl mx-auto md:px-6 lg:px-80">
-                <div class="flex justify-center" >
-                    <img height="100" width="100" src="https://static.ishtar-collective.net/icons/ishtar_on_blue.png"/>
-                </div>
-
-                <div class="mt-8 dark:bg-gray-800 text-white opacity-5 overflow-hidden shadow sm:rounded-lg">
-                    <div class="grid grid-cols-1 md:grid-cols-2">
-                        <div class="p-6" style="background-image: url(https://i.imgur.com/LYpIuYU.png); background-size:cover; background-position:right; background-repeat: no-repeat">
-                            <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://www.reddit.com/r/raidsecrets/" class="underline text-gray-900 dark:text-white">TUTORIALES</a></div>
-                            </div>
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-light dark:text-white text-lg background">
-                                    Aprende las principales mecánicas del juego, tips y guias para mazmorras e Incursiones!
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l" style="background-image: url(https://i.imgur.com/hoCk6By.png); background-size:cover; background-position:right; background-repeat: no-repeat">
-                            <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="/BuscadorItemsJuego" class="underline text-gray-900 dark:text-white">Galería de Equipamiento</a></div>
-                            </div>
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-light dark:text-white text-lg">
-                                    Inspecciona la historia y detalles de todas las armas, armaduras y accesorios del juego!
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="p-6 border-t border-gray-200 dark:border-gray-700" style="background-image: url(https://i.imgur.com/FNgw63T.png); background-size:cover; background-position:right; background-repeat: no-repeat">
-                            <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://www.ishtar-collective.net/timeline" class="underline text-gray-900 dark:text-white">Libros de Historias</a></div>
-                            </div>
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-light dark:text-white text-lg">
-                                    Lee todos los libros de Historia disponibles dentro del juego de esta temporada o de temporadas pasadas!
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l" style="background-image: url(https://i.imgur.com/4YW1t3F.png); background-size:cover; background-position:right; background-repeat: no-repeat">
-                            <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white"><a href="https://www.bungie.net/es-mx/News/Index" class="underline text-gray-900 dark:text-white">Historias de Bungie</a></div>
-                            </div>
-
-                            <div class="ml-12">
-                                <div class="mt-2 text-light dark:text-white text-lg">
-                                    Revisa todos los episodios de Lore Publicados por Bungie en <a href="https://www.bungie.net/" class="underline">Bungie.net</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div id="welcome_msg">
+            <p>Binenvenid@, esta página esta en construcción, lo que veas puede o no ser parte del producto final
+                Cualquier comentario es bienvenido! Espero disfrutes tu visita!
+            </p>
         </div>
+        <script>
+            var counter = setInterval(redirect, 500);
+            var seconds = 0;
+            function redirect (){
+                if(seconds === 3) {
+                    window.location.replace( "{{ url('/dashboard') }}");
+                }
+                seconds+=0.5;
+            }
+        </script>
     </body>
 </html>
